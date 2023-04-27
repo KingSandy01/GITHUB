998616e66ad363a4f78f3eb19b3dc509a086a897
KingSandy01 <ssatapathy90@gmail.com>
2023-04-27 06:13:58 +0530

    problem with soluion

--- py14nMore/py14WalrusOp.py
+++ py14nMore/py14WalrusOp.py
@@ -9,3 +9,13 @@ while True:
     heroes.append(getInput)
 
 print(heroes)
+
+#When any output shows this error------------------->
+# -----------------------------------------------------
+# >>> & C:/Users/HP/python/anaconda/python.exe "d:/python/vscode/python course/3. pandas/dataframes 231021.py"
+#   File "<stdin>", line 1
+#     & C:/Users/HP/python/anaconda/python.exe "d:/python/vscode/python course/3. pandas/dataframes 231021.py"
+#     ^
+# SyntaxError: invalid syntax
+# -----------------------------------------------------
+# Then delete the powershell multiple command windows and fresh run the program,, things will go smooth
